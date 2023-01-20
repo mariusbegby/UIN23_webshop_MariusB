429f19783b6f2b161aff198393d1d2d26addca8b
Marius Begby <mariusbegby@gmail.com>
2023-01-20 08:52:34 +0100

    Oppgave 1 til 3.1 ferdig

--- shopscript.js
+++ shopscript.js
@@ -8,22 +8,38 @@ function addToCart(title, price) {
     });
 
     renderCart();
+
+    if(document.querySelector("#cart-view").classList.contains("hidden")) {
+        console.log("test");
+        document.querySelector("#cart-view").classList.remove("hidden");
+    }
+}
+
+function deleteProduct(index) {
+    document.querySelector("#cart-view ul li:nth-child(" + (index + 1) + ")").classList.add("hidden");
+    setTimeout(() => {
+        cart.splice(index, 1);
+        renderCart();
+    }, 500);
 }
 
 function renderCart() {
     let listHTML = '';
+    let totalPrice = 0;
 
-    cart.map((prod) => {
+    cart.map((prod, index) => {
+        totalPrice += prod.productPrice;
         return (listHTML += `<li>
             <span class="title">${prod.productTitle}</span>
             <span class="price">${prod.productPrice},-</span>
             <span class="quantity">${prod.productQuantity}x</span>
-            <button class="delete">X</button>
+            <button class="delete" onclick="deleteProduct(${index})">X</button>
         </li>`);
     });
 
     document.querySelector("#cart-view > ul").innerHTML = listHTML;
     document.querySelector("#cart .label").innerHTML = cart.length;
+    document.querySelector("#total").innerHTML = totalPrice;
 }
 
 function toggleCart() {
